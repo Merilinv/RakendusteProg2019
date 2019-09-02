e51f6274416aa7fcc72cbad4598d71c9d137a790
Merilin Võrk <merilinv@tlu.ee>
2019-09-02 15:15:19 +0300

    Update lesson1.js

--- lesson1.js
+++ lesson1.js
@@ -1,1 +1,15 @@
 console.log("Hello World!");
+
+ function simpleArraySum (xs) {
+  let sum = 0;
+  
+  xs.forEach( (i)=>{
+    sum = sum + i;
+
+  })
+  return sum;
+}
+let a = simpleArraySum([1, 2, 6]);
+console.log(a);
+
+
