452f5eb52013a7b0d44ade0bb01a3d68b686db8d
Merilin Võrk <merilinv@tlu.ee>
2019-11-17 22:15:35 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,16 +3,19 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-require('dotenv').config();
 const itemRouter = require("./item.router.js");
 const userRouter = require("./user.router.js");
 const DB = require("./database.js");
 const Item = require("./item.model.js");
 const bodyParser = require("body-parser");
+/** Development environment. In Heroku we don't use .env file */
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+  }
 
 // ${DB_USERNAME}:${DB_PASSWORD} ... ${DB_NAME}
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-jis41.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; 
-console.log("DB", DB_URL);
+//console.log("DB", DB_URL);
 
 app.use(bodyParser.json());
 
